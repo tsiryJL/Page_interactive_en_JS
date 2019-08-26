39f9577ecb31813d7e8dcc632380f07995367b96
tsiry <YOUR@EMAIL.com>
2019-08-26 18:29:22 +0300

    final add

--- script.js
+++ script.js
@@ -96,3 +96,74 @@ let boutons= document.getElementsByClassName('btn-group');
 //===========================================================================================
 //fonctionalité n°7: si un utilisateur clique sur le bouton gris ==>, la dernière card (en bas à droite) va passer en premier 
 //===========================================================================================
+let parentCards = document.getElementsByClassName('col-md-4');
+let row = document.getElementsByClassName('row');
+let greyButton = document.getElementsByClassName('btn-secondary');
+
+greyButton.addEventListener('click',
+    function(e) {
+        e.preventDefault();
+        row.insertBefore(parentCards, parentCards[0]);
+    }
+);
+
+//==============================================================================================
+//Fonctionnalité n°8: la première card devra passer en dernier.
+//==============================================================================================
+let parentCards = document.getElementsByClassName('col-md-4');
+let blueButton = document.getElementsByClassName('btn-primary');
+let row = document.getElementsByClassName('row');
+let status = false;
+
+blueButton.addEventListener('click',
+    function(e){
+        if(status === false){
+            e.preventDefault();
+            row.appendChild(parentCards[0]);
+            status = true;
+        }
+        else{
+            e.preventDefault();
+            row.appendChild(parentCards[0]);
+            status = false;
+        }
+    }
+);
+
+//==================================================================================================
+//Fonctionalité n°9:  La fonctionnalité se déclenchera si le logo de la page (JS & Events) est sélectionné et qu'on appuie sur une touche spécifique du clavier.
+//==================================================================================================
+//    -Si l'utilisateur presse la touche "a", l'ensemble de la page va être condensé sur 4 colonnes Bootstrap à gauche de l'écran.
+//    -Si l'utilisateur presse la touche "y", l'ensemble de la page va être condensé sur 4 colonnes Bootstrap au milieu de l'écran.
+//    -Si l'utilisateur presse la touche "p", l'ensemble de la page va être condensé sur 4 colonnes Bootstrap à droite de l'écran.
+//    -Si l'utilisateur presse la touche "b", tout redevient normal.
+let logo = document.getElementsByClassName('navbar-brand')[0];
+let body = document.getElementsByTagName('body')[0];
+
+logo.addEventListener("keypress",
+    function(e){
+        e.preventDefault();
+        let x = e.key
+
+        if (x == "a" || x == "A"){
+            body.className = "";
+            body.classList.add("col-md-4");
+        } 
+        else if (x == "y" || x == "Y"){
+            body.className = "";
+            body.classList.add("col-md-4");
+            body.classList.add("offset-md-4");
+        } 
+        else if (x == "p" || x == "P"){
+            body.className = "";
+            body.classList.add("col-md-4");
+            body.classList.add("offset-md-8");
+        } 
+        else if (x == "b" || x == "B"){
+            body.className = "";
+        }
+    }
+);
+
+
+
